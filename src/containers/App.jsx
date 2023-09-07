f8b75a4125bce80c8dc45707379c173043b5ab29
Quadri Alameen <quadrameen@gmail.com>
2023-09-07 20:12:28 +0100

    added css

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,29 +1,46 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 import displayImage from '../olayinka.jpg'
 
 function App() {
+  const [utcTime, setUTCTime] = useState('');
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const currentUTCDate = new Date().toUTCString();
+      setUTCTime(currentUTCDate);
+    }, 1000); 
+    return () => clearInterval(intervalId);
+  }, []);
+
+    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const currentDate = new Date();
+    const currentDayOfWeek = daysOfWeek[currentDate.getDay()];
+
+  
+
   return (
-    <div>
+    <div className='container'>
       <header>
         <img src={displayImage} alt="olaQuadri" data-testid="slackDisplayImage"/>
-        <h1 data-testid="slackUserName">Slack Name: olaQuadri</h1>
       </header>
 
       <main>
-        <div data-testid="currentDayOfTheWeek">
-          Current Day Of The Week:
+      <h1 data-testid="slackUserName">Slack Name: olaQuadri</h1>
+        <div className='text' data-testid="currentDayOfTheWeek">
+          Current Day Of The Week: <strong>{currentDayOfWeek}</strong> 
         </div>
 
-        <div data-testid="currentUTCTime">
-          Current UTC Time:
+        <div className='text' data-testid="currentUTCTime">
+          Current UTC Time: <strong>{utcTime}</strong> 
         </div>
 
-        <div data-testid="myTrack">
-          Track: FrontEnd
+        <div className='text' data-testid="myTrack">
+          Track: <strong>FrontEnd</strong>
         </div>
 
-        <div data-testid="githubURL">
-          GitHub URL: https://github.com/Ari-o12code
+        <div className='text' data-testid="githubURL">
+          <a href="https://github.com/Ari-o12code/hngx-task-one">GitHub URL</a>
         </div>
         
       </main>
