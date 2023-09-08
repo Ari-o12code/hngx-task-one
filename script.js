a74f3746fb5a1f605d913073722d577c021808c2
Quadri Alameen <quadrameen@gmail.com>
2023-09-08 07:56:34 +0100

    fixed issue with displaying utc time

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 
-const currentDate = new Date();
-const dayOfWeekNumber = currentDate.getDay();
-const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-const currentDayOfWeek = daysOfWeek[dayOfWeekNumber];
+function updateDayOfTheWeek(){
+    const currentDate = new Date();
+    const dayOfWeekNumber = currentDate.getDay();
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const currentDayOfWeek = daysOfWeek[dayOfWeekNumber];
+    document.getElementById('currentDayOfTheWeek').innerHTML = `Current Day of the Week: <strong>${currentDayOfWeek}<strong>`
+}
 
+updateDayOfTheWeek()
 
-const timestamp = Date.now();
-const date = new Date(timestamp);
-const currentTime = date.toUTCString();
-
-document.getElementById('currentDayOfTheWeek').innerHTML = `Current Day of the Week: ${currentDayOfWeek}`
-
-const UtcTime = document.getElementById('currentUTCTime')
-UtcTime.textContent = `Current UTC Time: ${currentTime}`
+function updateUTCTime() {
+    const utcTimeElement = document.getElementById("currentUTCTime");
+    const timestamp = Date.now();
+    const currentUTCDate = new Date(timestamp).toUTCString();
+    utcTimeElement.innerHTML = `Current UTC Time: <strong>${currentUTCDate}<strong>`
+  }
+  updateUTCTime();
+  setInterval(updateUTCTime, 1000);
